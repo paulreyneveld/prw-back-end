bab99c3d9c069bd941137fe752096134b056201e
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-01-29 20:35:29 -0800

    Added environmental config for db access

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
+const password = process.env.DB_PASSWORD
+const url =
+  `mongodb+srv://prwUser:${password}@cluster0.4uagh.mongodb.net/blogs?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
 
 
 app.get('/', (request, response) => {
